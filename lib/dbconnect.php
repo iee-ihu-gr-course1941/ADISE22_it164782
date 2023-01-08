76b934b68a918125e917f62453f65a945f674bd3
cpavlidis <christos.pav98@gmail.com>
2023-01-08 16:38:53 +0200

    test commit

---
+++ lib/dbconnect.php
@@ -0,0 +1,20 @@
+<?php
+
+$host = 'localhost';
+$db = 'adise_project';
+require_once "db_upass.php";
+
+$user = $DB_USER;
+$pass = $DB_PASS;
+
+if (gethostname() == 'users.iee.ihu.gr') {
+    $mysqli = new mysqli($host, $user, $pass, $db, null, '/home/student/it/2016/it164782/mysql/run/mysql.sock');
+} else {
+    $mysqli = new mysqli($host, $user, $pass, $db);
+}
+
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: (" .
+    $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
+?>
